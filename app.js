caafdf70314eea6fef4d4c49982945b85c75f6e0
lite <lite.th745@gmail.com>
2023-01-19 12:00:52 +0530

    New commit

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}   
+}
 
 app.use(session(sessionConfig));
 app.use(flash());
